a024f9591b6172b239351efa9a2da2a79c6936f5
Lokesh Rengaraju <>
2025-04-15 12:02:42 -0700

    test(Functional Tests): TDD Chapter 2
    Adding the code for the comments, using unittest module.
    
    [ICTRAINING-1998]

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,46 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+# browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+
+        self.fail("Finsih the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+# Open the web application
+# Make sure you have the server running on localhost:8000
+# and the browser can access it.
+# browser.get("http://localhost:8000")
+
+# # Check the title of the page
+# assert "To-Do" in browser.title
+
+# # Entering the first to-do item
+
+# # when submitted it should, the page should refresh and show the item
+
+# # It should show the text box to add in the next item
+
+# # when submitted it should, the page should refresh and show both the items
+
+# # done entering the items
+
+# browser.quit()
 
