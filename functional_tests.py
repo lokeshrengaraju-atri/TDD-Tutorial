dfee4fb6896bd1b02bdbdab3597f025b41d4ef35
Lokesh Rengaraju <>
2025-04-16 10:15:42 -0700

    refactor(Functional Test): TDD Chapter 4
    Used selenium to refactor the test code.
    
    [ICTRAINING-2003]

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,11 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+
 
 # browser = webdriver.Firefox()
 
@@ -13,7 +19,26 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         self.browser.get("http://localhost:8000")
 
+        # Check the title and header of the page.
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do",header_text)
+
+        # check the input box, and what is the placeholder
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        # User enters the item in input box
+        inputbox.send_keys("Buy peacock feathers")
+
+        # When the user hits enter, the page updates and shows the item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1:Buy peacock feathers" for row in rows))
+
 
         self.fail("Finsih the test!")
 
