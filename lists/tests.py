2308966259eb8933efe44fa74dead6e3f713b652
Lokesh Rengaraju <>
2025-04-16 16:30:43 -0700

    feat(Lists App Model): TDD Chapter 5
    Model for list Items and associated migration
    
    [ICTRAINING-2018]

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 from django.http import HttpResponse
+from lists.models import Item
+
+
 
 
 
@@ -14,6 +17,27 @@ class HomePageTest(TestCase):
         response = self.client.post("/",data = {"item_text":"A new list item"})
         self.assertContains(response, "A new list item")
         self.assertTemplateUsed(response,"home.html")
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The First (ever) list item"
+        first_item.save()
+
+
+        second_item = Item()
+        second_item.text = "Item the Second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The First (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the Second")
+
+
 
 
 
