f05490cf1f4a2e099a8286745124aa65bd3a1205
Lokesh Rengaraju <>
2025-04-16 10:50:48 -0700

    feat(Lists App): TDD Chapter 4
    Refactor home page view to use a template.
    
    [ICTRAINING-2003]

--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,27 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.http import HttpResponse
+
 
 
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+
+    
     def test_home_page_returns_correct_html(self):
-        reponse = self.client.get("/")
-        self.assertContains(reponse,"<title>To-Do lists</title>")
-        self.assertContains(reponse, "<html>")
-        self.assertContains(reponse, "/<html>")
+        response = self.client.get("/")
+        self.assertContains(response,"<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response, "home.html")
+        # self.assertTemplateUsed(response, "wrong.html")
+    
+
+
 
 
 
