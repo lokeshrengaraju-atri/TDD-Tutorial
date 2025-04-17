34524f6746c3803e2db89ca2fb78167574e48ee4
Lokesh Rengaraju <>
2025-04-17 11:42:48 -0700

    feat(Lists App): TDD Chapter 7
    URLs for forms, moved code in views + tests, new URL
    
    [ICTRAINING-2035]

--- lists/views.py
+++ lists/views.py
@@ -10,10 +10,6 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/the-only-list-in-the-world/")
-    
     return render(request, "home.html")
 
 def view_list(request):
@@ -24,6 +20,11 @@ def view_list(request):
         "list.html",
         {"items":items},
     )
+
+def new_list(request):
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect("/lists/the-only-list-in-the-world/")
+
 
 
 
