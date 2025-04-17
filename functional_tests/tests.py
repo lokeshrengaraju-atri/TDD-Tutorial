f05ca7ed0f8632bb3f37daee5c0e38d2995e7bb6
Lokesh Rengaraju <>
2025-04-17 10:10:43 -0700

    test(Functional Tests): TDD Chapter 6
    replaced explicit waits with dynamic
    
    [ICTRAINING-2019]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,8 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase  
+from selenium.common.exceptions import WebDriverException
 
-
+MAX_WAIT = 5
 
 
 
@@ -20,10 +21,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME,"tr")
-        self.assertIn(row_text,[row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionError,WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+        
+
 
 
     def test_can_start_a_todo_list(self):
@@ -53,7 +64,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+    
         # self.check_for_row_in_list_table("1: Buy peacock feathers")
 
 
@@ -62,10 +75,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+       
 
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         
 
